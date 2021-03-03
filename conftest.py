db67d5c7f494998df46be2236d2908cf1ec3ea6b
Андрей <astran_86@mail.ru>
2021-03-03 23:57:26 +0300

    rel #1 каркас

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language',
+                     action='store',
+                     default='en',
+                     help='Choose language en, ru, (ect.)')
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    user_language = request.config.getoption('language')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    driver = webdriver.Chrome(options=options)
+    print("\nbrowser Chrome start...")
+    yield driver
+    driver.quit()
+    print("\nbrowser Chrome finish")
