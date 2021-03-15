5d40fe816402a45dbddd9b2400898ffd138fe8e1
astran_86 <andrew13579>
2021-03-15 17:44:39 +0300

    rel#  создание универсального теста с параметром

--- pages/locators.py
+++ pages/locators.py
@@ -11,7 +11,7 @@ class LoginPageLocators:
 
 
 class BasketAddPageLocators:
-    BOOK_NAME = (By.XPATH, "//div/h1")
+    BOOK_NAME = (By.XPATH, "//div/h1[contains(text(), '']")
     BOOK_NAME_ADD = (By.XPATH, "//div[@class='alertinner ']/strong")
     ADD_IN_BASKET = (By.XPATH, "//button[@value='Добавить в корзину']")
     BOOK_PRICE = (By.XPATH, '//div[@class=\'col-sm-6 product_main\']/p[@class=\'price_color\']')
--- test_product_page.py
+++ test_product_page.py
@@ -2,11 +2,24 @@ from .pages.product_page import ProductPage
 import time
 
 
-def test_guest_can_add_product_to_basket(browser):
+def test_guest_can_add_product_to_basket1(browser):
+    name = "The shellcoder's handbook"
+    price = "9,99 £"
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    page.book_in_basket()
-    page.book_price()
+    page.book_in_basket(name)
+    page.book_price(price)
+
+def test_guest_can_add_product_to_basket2(browser):
+    name = "Coders at Work"
+    price = "19,99 £"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019."
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.book_in_basket(name)
+    page.book_price(price)
