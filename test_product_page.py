77d10d07f40aa8ad74e6aa1ac5ebddf574a64afb
astran_86 <andrew13579>
2021-03-16 09:12:51 +0300

    rel#  создание универсального теста с параметризацией и меткой падающего теста

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,5 @@
 from .pages.product_page import ProductPage
-import time
+import pytest
 
 
 def test_guest_can_add_product_to_basket1(browser):
@@ -13,10 +13,32 @@ def test_guest_can_add_product_to_basket1(browser):
     page.book_in_basket(name)
     page.book_price(price)
 
+
 def test_guest_can_add_product_to_basket2(browser):
     name = "Coders at Work"
     price = "19,99 £"
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019."
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.book_in_basket(name)
+    page.book_price(price)
+
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket3(browser, link):
+    name = "Coders at Work"
+    price = "19,99 £"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
